528728b440392f3b500ead9ae22954b2d80d5a38
giorgi473 <giorgi.kavtaradze2000@mail.ru>
2025-12-24 22:27:53 +0400

    remove SafeAreaProvider component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,11 @@
 import { Stack } from "expo-router";
-import { View } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const RootLayout = () => {
   return (
-    <SafeAreaProvider>
-      <View style={{ flex: 1, backgroundColor: "#000", paddingVertical: 1 }}>
-        <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
-          <Stack screenOptions={{ headerShown: false }} />
-        </SafeAreaView>
-      </View>
-    </SafeAreaProvider>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeAreaView>
   );
 };
 
